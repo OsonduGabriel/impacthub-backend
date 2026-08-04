e5248a1803a7646b92de9e29ed12a398241aa2e4
Osondu Gabriel <136438699+OsonduGabriel@users.noreply.github.com>
2026-08-04 19:10:48 +0100

    fixed authcontroller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -10,7 +10,7 @@ const authService = new AuthService();
 //
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRATION,
+    expiresIn: process.env.JWT_EXPIRES_IN,
   });
 };
 const generateOTP = (length = 6) => {
